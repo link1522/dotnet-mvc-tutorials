c0d29d6db7a84d9f8ccc836177b90d99993e920c
terry <terry.lin@habook.com.tw>
2024-08-16 14:29:16 +0800

    first commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers;
+
+public class HelloController : Controller
+{
+  private readonly ILogger<HelloController> _logger;
+
+  public HelloController(ILogger<HelloController> logger)
+  {
+    _logger = logger;
+  }
+
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  public IActionResult Welcome(string name, int numTimes = 1)
+  {
+    ViewData["Message"] = "Hello " + name;
+    ViewData["NumTimes"] = numTimes;
+    return View();
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
+}
